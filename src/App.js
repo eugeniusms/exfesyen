e6a873c7f38775aecea697f18039492bc8dabc01
eugeniusms <eugeniusms@gmail.com>
2022-07-14 03:14:48 +0700

    Initializing FOOTER PRESET

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Button from './components/Button';
 import Card from './components/Card';
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <div className="bg-creamy">
       <Navbar />
-        <div className="flex justify-center p-6 gap-4">
+        <div className="flex justify-center p-6 gap-4 min-h-screen">
           <Card>
             <div className="flex justify-center p-6 gap-4">
               <Button preset="cancel">Cancel</Button>
@@ -24,6 +25,7 @@ function App() {
             </div>
           </Card>
         </div>
+      <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "../Button";
+import { CameraIcon, LinkIcon, GlobeIcon, AdjustmentsIcon } from "@heroicons/react/outline";
+
+const Footer = () => {
+    return (
+        <div className="bg-navy rounded-t-2xl py-6 px-12">
+        <div className="flex justify-center">
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col text-creamy max-w-2xl gap-4">
+                    <h1 className="text-2xl">Exfesyen</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet metus ac nisi pretium sagittis ac vitae erat. Nunc tincidunt enim vitae tristique ultrices.</p>
+                </div>
+            </div>
+        </div>
+        <div className="flex justify-between">
+            <div className="flex justify-center text-armyLight gap-6">
+                <Button preset="transparent">About Us</Button>
+                <Button preset="transparent">Gallery</Button>
+                <Button preset="transparent">Contact</Button>
+            </div>
+            <div className="flex justify-center text-armyLight gap-6">
+                <CameraIcon className="w-6 h-6"/>
+                <LinkIcon className="w-6 h-6"/>
+                <GlobeIcon className="w-6 h-6"/>
+                <AdjustmentsIcon className="w-6 h-6"/>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Footer;
