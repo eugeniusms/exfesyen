915fd0fa09916bdf2df7d91c7b54bf307929bd04
eugeniusms <eugeniusms@gmail.com>
2022-07-15 05:34:25 +0700

    Clearing Issue : Not Auto Responsive with Gadget

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,19 @@ import NavbarDesktop from "./NavbarDesktop";
 import NavbarMobile from "./NavbarMobile";
 
 const Navbar = () => {
-    const [isMobile, setIsMobile] = useState(false);
+    // SOLUTION AUTO RESPONSIVE WITH GADGET
+    // Using getter size of window width to first state
+    const getWindowSize = () => {
+        if (window.innerWidth < 768) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const [isMobile, setIsMobile] = useState(getWindowSize);
  
-    //choose the screen size 
+    //choose the screen size
     const handleResize = () => {
         if (window.innerWidth < 768) {
             setIsMobile(true)
