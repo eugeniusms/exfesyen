6b1c85151e2b04051eb66c22edbb0bc9ba85e62c
eugeniusms <eugeniusms@gmail.com>
2022-07-15 22:18:37 +0700

    Success Fetching Fake API

--- src/pages/catalog/catalogPrice.js
+++ src/pages/catalog/catalogPrice.js
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
-import React from 'react';
 import ArrowRD from '../../components/ArrowRD';
 import Button from '../../components/Button';
 import Content from '../../components/Fonts/Content';
 
 const CatalogPrice = () => {
+  const API_URL = "https://fakestoreapi.com/products/"
+
+  const [ data, setData ] = useState([]);
+  const [ fetchError, setFetchError ] = useState(null);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        console.log(listItems);
+        setData(listItems)
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchData())();
+    },2000)
+  }, [])
+
   return (
     <div>
         <div className="gap-4 min-h-screen">
@@ -16,6 +40,14 @@ const CatalogPrice = () => {
               <Button preset="sortby-left">SORT BY</Button>
               <Button preset="sortby-right">FILTERS</Button>
             </div>
+            {/* WOMEN CLOTH */}
+            {data.map((item) => (
+              item.category === "women's clothing" && <img src={item.image} alt="image"/> 
+            ))}
+            {/* MEN CLOTH */}
+            {data.map((item) => (
+              item.category === "men's clothing" && <img src={item.image} alt="image"/> 
+            ))}
         </div>
     </div>
   );
